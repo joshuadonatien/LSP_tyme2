a672c55f5588df92f3a67bd9cec96fe39b5e6136
joshuadonatien <147733641+joshuadonatien@users.noreply.github.com>
2025-01-23 15:18:20 -0500

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,6 +7,7 @@ package org.howard.edu.lsp.assignment1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World!!");
 		// TODO Auto-generated method stub
 
 	}
